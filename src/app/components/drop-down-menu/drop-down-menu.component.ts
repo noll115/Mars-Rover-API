89fcfb5356f58992ca051f3206b0445f17952d1c
Noel Gomez <nollgmz4@gmail.com>
2020-02-03 00:14:35 -0800

    finished dropdown

--- src/app/components/drop-down-menu/drop-down-menu.component.ts
+++ src/app/components/drop-down-menu/drop-down-menu.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, EventEmitter, SimpleChanges, Output } from '@angular/core';
 
 @Component({
   selector: 'app-drop-down-menu',
   templateUrl: './drop-down-menu.component.html',
   styleUrls: ['./drop-down-menu.component.scss']
 })
-export class DropDownMenuComponent implements OnInit {
+export class DropDownMenuComponent implements OnInit, OnChanges {
   @Input() choices: string[];
-  @Input() checkBoxes: boolean = false;
   @Input() label: string;
   @Input() boxText: string;
+  @Output() selected = new EventEmitter<string[]>();
   currentChoice: String = "";
-  numOfChoicesChosen: number = 0;
-  dropDown:boolean = false;
+  numOfChoicesChosen: number;
+  dropDown: boolean = false;
+  chosenArray: string[];
+
   constructor() { }
 
   ngOnInit() {
+    this.numOfChoicesChosen = this.choices.length;
+    this.SelectAll();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+
   }
 
   DropMenu() {
@@ -23,14 +33,31 @@ export class DropDownMenuComponent implements OnInit {
     this.dropDown = !this.dropDown;
   }
 
-  ChoiceSelect(index:number){
-    console.log(index);
+  ChoiceSelect(index: number, event) {
+    if (event.target.tagName === "INPUT") {
+      let arrIndex = this.chosenArray.indexOf(this.choices[index]);
+      if (arrIndex !== -1) {
+        this.chosenArray.splice(arrIndex, 1);
+      } else {
+        this.chosenArray.push(this.choices[index]);
+      }
+      this.numOfChoicesChosen = this.chosenArray.length;
+      this.selected.emit(this.chosenArray);
+    }
   }
 
-  ClearSelection(){
-
+  isChecked(index: number) {
+    return this.chosenArray.includes(this.choices[index]);
   }
-  SelectAll(){
-    
+
+  ClearSelection() {
+    this.numOfChoicesChosen = 0;
+    this.chosenArray = [];
+  }
+  SelectAll() {
+
+    this.chosenArray = [...this.choices];
+    this.numOfChoicesChosen = this.chosenArray.length;
+    this.selected.emit(this.chosenArray);
   }
 }
