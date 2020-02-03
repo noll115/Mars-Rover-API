1195583ee4beed1c15dbe87f1d1933db7b2c11bc
Noel Gomez <nollgmz4@gmail.com>
2020-02-03 09:05:26 -0800

    Able to get rover cam as enum

--- src/app/models/rover-data.ts
+++ src/app/models/rover-data.ts
@@ -1,9 +1,22 @@
-interface RoverCam {
+export enum RoverCameras {
+    FHAZ = "FHAZ",
+    RHAZ = "RHAZ",
+    MAST = "MAST",
+    CHEMCAM = "CHEMCAM",
+    MAHLI = "MAHLI",
+    MARDI = "MARDI",
+    NAVCAM = "NAVCAM",
+    PANCAM = "PANCAM",
+    MINITES = "MINITES"
+}
+
+
+interface CamName {
     name: string,
     full_name: string,
 }
 
-interface RoverCams extends Array<RoverCam> { }
+interface CamNames extends Array<CamNames> { }
 
 export interface RoverData {
     id: Number,
@@ -14,5 +27,5 @@ export interface RoverData {
     max_sol: number,
     max_date: string,
     total_photos: Number,
-    cameras: RoverCams
+    cameras: CamNames
 }
