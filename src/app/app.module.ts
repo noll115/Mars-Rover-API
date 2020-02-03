4b5d6a4515c2d841c0225d583a2454c16c501fca
Noel Gomez <nollgmz4@gmail.com>
2020-02-03 00:15:42 -0800

    started sol input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoCardComponent } from './components/photo-card/photo-card.component';
 import { HttpClientModule } from "@angular/common/http";
 import { DropDownMenuComponent } from './components/drop-down-menu/drop-down-menu.component';
+import { NumberInputComponent } from './components/number-input/number-input.component';
+import { FormsModule } from "@angular/forms";
 
 
 @NgModule({
@@ -13,11 +14,14 @@ import { DropDownMenuComponent } from './components/drop-down-menu/drop-down-men
     AppComponent,
     DropDownMenuComponent,
     PhotoCardComponent,
+    NumberInputComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/number-input/number-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-number-input',
+  templateUrl: './number-input.component.html',
+  styleUrls: ['./number-input.component.scss']
+})
+export class NumberInputComponent implements OnInit {
+  @Input() label = "test";
+  @Input() maxNum: number = 0;
+  inputNumber: number = 0;
+  @Output() submit = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  OnInput(event) {
+    let character = event.charCode;
+    if (character == 101 || character == 69 || character == 46 || character == 43 || character == 45) {
+      return false;
+    }
+    return true;
+  }
+
+  OnSubmit() {
+    if (this.inputNumber > this.maxNum) {
+      this.inputNumber = this.maxNum;
+    }
+    this.submit.emit(this.inputNumber);
+  }
+}
