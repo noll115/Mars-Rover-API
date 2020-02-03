60a4e71ebdc210516adb365e94ea9b6426f1045a
Noel Gomez <nollgmz4@gmail.com>
2020-02-03 09:05:15 -0800

    added ability to get photos from a camera on sol

--- src/app/services/nasa.service.ts
+++ src/app/services/nasa.service.ts
@@ -4,7 +4,8 @@ import { map } from "rxjs/operators";
 import { Rovers } from "../models/rovers.enum";
 import { SolCameraData } from "../models/sol-camera-data";
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { RoverData } from '../models/rover-data';
+import { RoverData, RoverCameras } from '../models/rover-data';
+import { CamPhotoData } from '../models/cam-photo-data';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,9 @@ export class NASAService {
   constructor(private http: HttpClient) { }
 
   public getSolData(rover: Rovers): Observable<SolCameraData> {
-    let manifestURL = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${this.api_key}`
-    return this.http.get(manifestURL)
+    let manifestURL = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}`
+    let params: HttpParams = new HttpParams({ fromObject: { api_key: this.api_key } })
+    return this.http.get(manifestURL, { params })
       .pipe(map(res => {
         // sols are not 1 to 1 to array indexes some sols are missing
         let photos: any[] = res['photo_manifest']['photos']
@@ -29,9 +31,22 @@ export class NASAService {
 
   public getRoverData(rover: Rovers): Observable<RoverData> {
     let roverURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover.toLowerCase()}/`;
-    let httpParams: HttpParams = new HttpParams({ fromObject: { api_key: this.api_key } })
-    return this.http.get(roverURL, { params: httpParams }).pipe(map(res => {
+    let params: HttpParams = new HttpParams({ fromObject: { api_key: this.api_key } })
+    return this.http.get(roverURL, { params }).pipe(map(res => {
       return (res['rover'] as RoverData)
+    }));
+  }
+  //should only be called when there are photos
+  public getPhotos(rover: Rovers, sol: number, camera: RoverCameras): Observable<CamPhotoData> {
+    let photoURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos`
+    let params: HttpParams = new HttpParams({ fromObject: { api_key: this.api_key, sol: sol.toString(), camera } })
+    return this.http.get(photoURL, { params }).pipe(map(res => {
+      let photos: any[] = res['photos'];
+      let camData: CamPhotoData = {
+        numOfPhotos: photos.length,
+        sampleImg: photos[0]['img_src']
+      };
+      return camData;
     }));
   }
 }
